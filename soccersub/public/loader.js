a8de2856a46faf84b0f92a0f3609c421259037de
Joshua Marantz <jmarantz@gmail.com>
2017-11-18 11:36:10 -0500

    commit the loader script

---
+++ soccersub/public/loader.js
@@ -0,0 +1,9 @@
+goog.loadModuleFromUrl("js/lineup.js");
+goog.loadModuleFromUrl("js/util.js");
+goog.loadModuleFromUrl("js/player.js");
+goog.loadModuleFromUrl("js/position.js");
+goog.loadModuleFromUrl("js/map_section.js");
+goog.loadModuleFromUrl("js/array_section.js");
+goog.loadModuleFromUrl("js/storage.js");
+goog.loadModuleFromUrl("js/game.js");
+goog.loadModuleFromUrl("js/main.js");
