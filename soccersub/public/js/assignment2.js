8938c94e5229949100e4f943c6bdd9168667fbf9
Joshua Marantz <jmarantz@gmail.com>
2017-12-25 08:48:26 -0500

    In-progress second generation planning infrastructure.

---
+++ soccersub/public/js/assignment2.js
@@ -0,0 +1,21 @@
+goog.module('soccersub.Assignment2');
+
+/**
+ * Captures an assignment that we want to make. Players are specified as
+ * indexes into this.players_.  The critical design question here is what
+ * happens to automated and manually specified assignments when:
+ *   - a player is swapped or renamed.  In this case, all assignments are
+ *     retained except for the even swap.
+ *   - players are removed or added.  In this case, the default assignments
+ *     are recomputed from scratch based on the player ordering.  We try to
+ *     keep the ordering as stable as possible.
+ *
+ * @typedef {!{
+ *   playerName: string,
+ *   positionName: string,
+ *   timeSec: number,
+ * }}
+ */
+let Assignment2;
+
+exports = Assignment2;
