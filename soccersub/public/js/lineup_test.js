145f7017243ab211de05fc9801d1e1953e9c4320
Joshua Marantz <jmarantz@gmail.com>
2017-12-28 15:22:32 -0500

    Pull in the testsuite library in a goog.require in the _test.html file, to avoid the onload hack in the _test.js file.

--- soccersub/public/js/lineup_test.js
+++ soccersub/public/js/lineup_test.js
@@ -6,9 +6,9 @@ goog.module('soccersub.Lineup_test');
 goog.setTestOnly('soccersub.Lineup_test');
 
 const Lineup = goog.require('soccersub.Lineup');
-goog.require('goog.testing.testSuite');
+const testSuite = goog.require('goog.testing.testSuite');
 
-window.onload = () => goog.testing.testSuite({
+testSuite({
   testConstruction() {
     const lineup = new Lineup(5, ['jim', 'joe', 'fred', 'harvey', 'frank']);
     assertTrue(lineup.playerNames.has('jim'));
