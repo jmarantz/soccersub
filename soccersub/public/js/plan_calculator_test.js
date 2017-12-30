6b8767a8f298c24f0cd33f78684455d3b7e3fdd2
Joshua Marantz <jmarantz@gmail.com>
2017-12-30 11:45:53 -0500

    add plan calculator test file to repo

---
+++ soccersub/public/js/plan_calculator_test.js
@@ -0,0 +1,18 @@
+goog.module('soccersub.PlanCalculatorTest');
+
+const Lineup = goog.require('soccersub.Lineup');
+const PlanCalculator = goog.require('soccersub.PlanCalculator');
+const util = goog.require('soccersub.util');
+
+exports = {
+  'getTestName': () => {
+    return 'PlanCalculatorTest';
+  },
+
+  'testConstruction': () => {
+    const lineup = new Lineup(5, ['jim', 'joe', 'fred', 'harvey', 'frank']);
+    const calculator = new PlanCalculator(lineup, () => {}, console.log);
+    assertEquals(5, calculator.updatePlayers());
+    assertEquals(0, calculator.updatePlayers());
+  },
+};
